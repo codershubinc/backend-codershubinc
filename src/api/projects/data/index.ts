d7f7269c4cec3ab32caee9042e18eb5810543262
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-09-25 00:34:45 +0530

    feat: add vs-music project data to projectsData

--- src/api/projects/data/index.ts
+++ src/api/projects/data/index.ts
@@ -1,7 +1,9 @@
 import githubNewtab from "./githubNewtab";
+import vsMusic from "./vs-music";
 
 export const projectsData = {
-    "githubNewtab": githubNewtab
+    "githubNewtab": githubNewtab,
+    "vs-music": vsMusic
 }
 
 export const projectList = Object.keys(projectsData);
